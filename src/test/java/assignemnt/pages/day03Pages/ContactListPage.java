ef4549e08160173de744dea7eff05725888b9c47
ZahraAleid <zahra.eid112@gmail.com>
2025-10-05 06:31:57 +0300

    30/9/2025

---
+++ src/test/java/assignemnt/pages/day03Pages/ContactListPage.java
@@ -0,0 +1,29 @@
+package assignemnt.pages.day03Pages;
+
+import assignemnt.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class ContactListPage {
+
+    public ContactListPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "add-contact")
+    public WebElement addContact;
+
+    @FindBy(id = "logout")
+    public WebElement logout;
+
+    @FindBy(xpath = "//td[2]")
+    public List<WebElement> dataCount;
+
+    @FindBy(xpath = "//table//tr//td[4]")
+    public List<WebElement> emailColumn;
+
+
+}
